88f9101bf6e472c0d2e6fe7ce569ea7ca21a103b
luxardOri <127153726+luxardOri@users.noreply.github.com>
2023-08-23 19:58:31 -0700

    adding in graphql stuff

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -2,18 +2,31 @@ const { User, Message, Chat } = require('../models');
 
 const resolvers = {
     Query: {
-        user: async () => {
+        getAllUsers: async () => {
             return User.find({});
-          },
-        messages: async (parent,args) => {
-            const message = await Message.findById(args.id)
-            return message;
+        },
+        getMessages: async (parent,args) => {
+            const chat = await Chat.findById(args.id)
+            return chat.messages;
+        },
+        getAllChats: async () => {
+            return Chat.find({});
         }
-
     },
     Mutation: {
         // at least one for posting message
-
+        createMessage: async (parent, args) => {
+            const message = await Message.create(args);
+            return message;
+        },
+        createUser: async (parent,args) => {
+            const user = await User.create(args);
+            return user;
+        },
+        createChat: async (parent,args) => {
+            const chat = await Chat.create(args);
+            return chat;
+        }
     }
 };
 
